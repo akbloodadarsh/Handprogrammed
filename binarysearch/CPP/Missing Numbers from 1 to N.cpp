0ab4bd0f0ece009695a01d0612135e33ec1bf7fd
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-10-07 15:27:33 +0530

    Update Missing Numbers from 1 to N.cpp

--- binarysearch/CPP/Missing Numbers from 1 to N.cpp
+++ binarysearch/CPP/Missing Numbers from 1 to N.cpp
@@ -1,3 +1,10 @@
+// Time:- O(N)
+// Space:- O(1) (No extra space for return vector)
+
+// Firstly, It is very intuitive to come up with using O(N) space to store the information.
+// e.g. [ 4, 3, 2, 7, 8, 2, 3, 1] we can use a binary array (O(N) space) to count it. [ 1, 1, 1, 1, 0, 0, 1, 1]
+// Note that it has the same amount of information with [ -, -, -, -, +, +, -, -] . You can just attach this information to the original array without generating a new array. That's the whole point.
+
 #include "solution.hpp"
 using namespace std;
 
